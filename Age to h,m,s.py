54648f796a948f75dc6df5f4c61189429aba150f
h-yaz01 <hosseinyazdanizero@gmail.com>
2025-11-17 18:03:06 +0330

    age to hours, minutes, seconds

---
+++ Age to h,m,s.py
@@ -0,0 +1,10 @@
+years = int(input("input age: ""))
+
+days = years * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print("hours: ", hours)
+print("minutes: ", minutes)
+print("seconds: ", seconds)
